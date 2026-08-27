532511c0980e908af4f7964784eb9a839b06b186
Aamir-choudhary <amhod5683@gmail.com>
2026-08-27 12:15:58 +0530

    account_add

--- src/components/footer/FooterBottomBar.jsx
+++ src/components/footer/FooterBottomBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SOCIAL_ICONS = [
   {
     name: 'Instagram',
-    href: '#instagram',
+    href: 'https://www.instagram.com/styleincar',
     svg: (
       <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
         <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
@@ -12,7 +12,7 @@ const SOCIAL_ICONS = [
   },
   {
     name: 'Facebook',
-    href: '#facebook',
+    href: 'https://www.facebook.com/profile.php?id=61587131289144',
     svg: (
       <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
         <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
@@ -20,8 +20,17 @@ const SOCIAL_ICONS = [
     ),
   },
   {
+    name: 'X',
+    href: 'https://x.com/StyleinCar',
+    svg: (
+      <svg className="w-3.5 h-3.5 fill-current" viewBox="0 0 24 24">
+        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+      </svg>
+    ),
+  },
+  {
     name: 'LinkedIn',
-    href: '#linkedin',
+    href: 'https://www.linkedin.com/in/stylein-car-6a65b93b4/',
     svg: (
       <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
@@ -29,20 +38,20 @@ const SOCIAL_ICONS = [
     ),
   },
   {
-    name: 'TikTok',
-    href: '#tiktok',
+    name: 'YouTube',
+    href: 'https://www.youtube.com/@StyleinCar',
     svg: (
       <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
-        <path d="M12.525.02c1.31 0 2.6.43 3.65 1.22.42-1.02 1.4-1.24 1.4-1.24v3.91s-1.24-.03-2.39-.77c0 2.21 0 4.41-.01 6.62 0 3.73-3.03 6.75-6.75 6.75-3.73 0-6.75-3.02-6.75-6.75 0-3.72 3.02-6.74 6.75-6.74.34 0 .68.03 1.01.08v3.9c-.31-.08-.65-.12-.99-.12-1.63 0-2.95 1.32-2.95 2.95 0 1.63 1.32 2.95 2.95 2.95 1.63 0 2.95-1.32 2.95-2.95V.02h2.13z"/>
+        <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
       </svg>
     ),
   },
   {
-    name: 'YouTube',
-    href: '#youtube',
+    name: 'Pinterest',
+    href: 'https://in.pinterest.com/styleincaar/styleincar-premium-car-detailing-mods/',
     svg: (
       <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
-        <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+        <path d="M12 0C5.373 0 0 5.372 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738.098.119.112.224.083.345-.09.375-.291 1.199-.334 1.357-.053.225-.178.271-.409.165-1.522-.709-2.472-2.936-2.472-4.72 0-3.843 2.792-7.371 8.053-7.371 4.227 0 7.515 3.011 7.515 7.041 0 4.202-2.65 7.582-6.326 7.582-1.235 0-2.397-.642-2.793-1.402l-.76 2.898c-.276 1.063-1.024 2.395-1.526 3.207C9.308 23.895 10.634 24 12 24c6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z"/>
       </svg>
     ),
   },
@@ -51,25 +60,27 @@ const SOCIAL_ICONS = [
 export default function FooterBottomBar() {
   return (
     <div className="pt-6 border-t border-white/10 flex flex-col sm:flex-row items-center justify-between gap-4 text-[0.8rem] text-neutral-400 font-body relative z-20">
-      {/* Copyright & Legal Links */}
+      {/* Copyright & Legal */}
       <div className="flex items-center gap-6 flex-wrap justify-center sm:justify-start">
         <span>© {new Date().getFullYear()} STYLEIN. All Rights Reserved</span>
-        <a href="#terms" className="text-neutral-400 hover:text-white no-underline transition-colors">
+        <span className="text-neutral-500 hover:text-neutral-300 transition-colors">
           Terms
-        </a>
-        <a href="#privacy" className="text-neutral-400 hover:text-white no-underline transition-colors">
+        </span>
+        <span className="text-neutral-500 hover:text-neutral-300 transition-colors">
           Privacy
-        </a>
+        </span>
       </div>
 
-      {/* Social Media SVG Icons */}
-      <div className="flex items-center gap-4">
+      {/* Official Social Media Links */}
+      <div className="flex items-center gap-4 flex-wrap justify-center">
         {SOCIAL_ICONS.map((soc) => (
           <a
             key={soc.name}
             href={soc.href}
+            target="_blank"
+            rel="noopener noreferrer"
             aria-label={soc.name}
-            className="text-neutral-400 hover:text-white transition-colors no-underline cursor-pointer"
+            className="text-neutral-400 hover:text-stylein-red hover:scale-115 transition-all duration-200 no-underline cursor-pointer"
           >
             {soc.svg}
           </a>
